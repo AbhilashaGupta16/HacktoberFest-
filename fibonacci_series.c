cab8c2c724be5c5ee6359689ac59fb82e2cd500e
Amisha Singh <59981201+28-2001@users.noreply.github.com>
2020-10-03 16:45:53 +0530

    Add files via upload

---
+++ fibonacci_series.c
@@ -0,0 +1,25 @@
+//Program to print the Fibonacci series using recursion.
+#include <stdio.h>
+int Fibonacci(int);
+int main()
+{
+    int n, i = 0, res;
+    printf("Enter the number of terms\n");
+    scanf("%d",&n);
+    printf("Fibonacci series\n");
+    for(i = 0; i < n; i++ )
+    {
+        res = Fibonacci(i);
+        printf("%d\t",res);
+    }
+    return 0;
+}
+int Fibonacci(int n)
+{
+    if ( n == 0 )
+        return 0;
+    else if ( n == 1 )
+        return 1;
+    else
+        return ( Fibonacci(n-1) + Fibonacci(n-2) );
+}
